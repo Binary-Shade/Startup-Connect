208887fadb0d3c4d1b6e368a52b30f8d72511de5
suresh k <binaryshade@binaryshade.binaryshade>
2024-12-06 14:09:03 +0530

    bugs fixed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -16,7 +16,7 @@ export default async function Home({searchParams}: {
   return (
     <>
       <section className="pink_container">
-      <h2 className="heading">Pitch your startup, <br /> conenct with entrepreneurs</h2>
+      <h2 className="heading">Pitch your startup, <br /> connect with entrepreneurs</h2>
       <p className="sub-heading !max-w-3xl">submit ideas ,vote on pitches, get noticed in virtual competition.</p>
       <SearchForm query={query} />
     </section>
